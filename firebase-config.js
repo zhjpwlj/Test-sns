bba282ebc860cf15eac693963dd4030c1297065b
zhjpwlj <141892783+zhjpwlj@users.noreply.github.com>
2025-05-29 17:28:50 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHpk7epCOFF-jP0wsTmIpIWCfHz866UQc",
+  authDomain: "login-48c5e.firebaseapp.com",
+  projectId: "login-48c5e",
+  storageBucket: "login-48c5e.firebasestorage.app",
+  messagingSenderId: "1039778358572",
+  appId: "1:1039778358572:web:8c0b92af388367ef6e7889",
+  measurementId: "G-FJ4ZKKJ4RC"
+};
